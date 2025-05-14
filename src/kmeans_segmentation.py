48adf3f3cf71ce97f198f19bb0a69b0d534de360
luccahuguet <lucca.huguet2@gmail.com>
2025-05-14 16:52:38 -0300

    initial commit

--- src/kmeans_segmentation.py
+++ src/kmeans_segmentation.py
@@ -14,7 +14,7 @@ output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
 # Set your input image filename here
-input_image_filename = "drone_image.jpg"
+input_image_filename = "input/aerial.jpg" 
 image_path = os.path.join(input_dir, input_image_filename)
 
 # Load DINOv2 model
