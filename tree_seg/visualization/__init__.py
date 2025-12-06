5a63b670787a4f7ee0b0e6d91acedfe99203f0aa
luccahuguet <lucca.huguet2@gmail.com>
2025-12-06 15:25:43 -0300

    Expose eval panels in visualization package

--- tree_seg/visualization/__init__.py
+++ tree_seg/visualization/__init__.py
@@ -1,7 +1,9 @@
 # Modern visualization modules
 
 from .plotting import generate_visualizations
+from .eval_panels import plot_evaluation_comparison
 
 __all__ = [
-    'generate_visualizations'
-] 
+    "generate_visualizations",
+    "plot_evaluation_comparison",
+]
